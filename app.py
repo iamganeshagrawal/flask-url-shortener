28a257e05002417642439e9b543e84eac66b9ac2
Ganesh Agrawal <gmegotiya@gmail.com>
2020-08-21 18:34:11 +0530

    Post and GET method and Redirect

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Ganesh')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return "This is a url shortener"
+
+@app.route("/your-url", methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        # slug = request.args['slug'] # GET 
+        # url = request.args['url']   #GET
+        slug = request.form['slug'] # POST 
+        url = request.form['url']   #POST
+        return render_template('your_url.html', slug=slug, url=url)
+    else:
+        # return render_template('home.html')
+        return redirect('/')    #Redirect to home
