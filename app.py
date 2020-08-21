def235319705f990b76df58c58b1d94ca429492d
Ganesh Agrawal <gmegotiya@gmail.com>
2020-08-21 18:57:06 +0530

    Redirect with flash message and secrect_key

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+import os.path
 
 app = Flask(__name__)
+app.secret_key = 'h55dasdash3hacas68789cxaj'
 
 @app.route('/')
 def home():
@@ -17,6 +20,22 @@ def your_url():
         # url = request.args['url']   #GET
         slug = request.form['slug'] # POST 
         url = request.form['url']   #POST
+
+        # Storing data into json
+        urls = {}
+        
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+        
+        if slug in urls.keys():
+            flash('The short name has already been taken. Please select another one')
+            return redirect(url_for('home'))
+
+        urls[slug] = {'url': url}
+        with open('urls.json', 'w') as urls_file:
+            json.dump(urls, urls_file)
+
         return render_template('your_url.html', slug=slug, url=url)
     else:
         # return render_template('home.html')
