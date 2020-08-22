15ea957ce94f29b6ae8885ce61141381b2c3f4b8
Ganesh Agrawal <gmegotiya@gmail.com>
2020-08-22 17:32:31 +0530

    pytest added for basic testing

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 from urlshort import create_app
 
 def test_shorten(client):
+    # Testing Home Page
     response = client.get('/')
+    #Checking Shorten word is avaibale on page or not
     assert b'Shorten' in response.data
